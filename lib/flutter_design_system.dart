0b42ffc71bb1b8dbc5a96e5debddc08e41085d18
dellarosamarco <dellarosamarco00@gmail.com>
2024-10-14 22:24:31 +0200

    feat: library

---
+++ lib/flutter_design_system.dart
@@ -0,0 +1,9 @@
+library flutter_design_system;
+
+export './components/custom_bottom_navigation_bar.dart';
+export './components/custom_card.dart';
+export './components/custom_modal.dart';
+export './components/floating_popup_menu.dart';
+export './components/items_grid.dart';
+export './components/items_showcase.dart';
+export './components/text_editor.dart';
